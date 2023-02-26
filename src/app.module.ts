c736bf9f6af6f27e1e7b3137fddd856fa05fe8d0
boburf <changeitgo3@gmail.com>
2023-02-26 12:30:06 +0500

    fix: db

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { DBModule } from './db/db.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import configService from './ormconfig';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { ConfigModule } from '@nestjs/config';
     AlbumModule,
     TrackModule,
     FavoriteModule,
+    AuthModule
   ],
 })
 export class AppModule {}
